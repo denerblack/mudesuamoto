374e148a56d661a9147d7b9e89a989721d9651ea
denerblack <dener.wilian@gmail.com>
2011-07-24 21:11:05 -0400

    Limpando o branch

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Mudesuamoto::Application.config.secret_token = '77d5baf318772dd7ee3a079951317f93d1b3490c6747045e99d3e32f37658921af4498193dfc5c939fb3f15d7dc423aac7f3cbf10e7ff508951dc3040b3ad7f6'
