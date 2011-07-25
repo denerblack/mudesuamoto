8938ad943b15bc57380c69f5059d46e65a1e641c
denerblack <dener.wilian@gmail.com>
2011-07-25 00:07:49 -0400

    Primeiro commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mudesuamoto::Application.config.secret_token = '86a9636f96a0ab4f7c1f2974c2353426f4008efa7283aae3fb8a44b23876f4c85318847b91b4b56672ee4f08131d2c0ffcac263152ab73576e74f78841f6a5f1'
